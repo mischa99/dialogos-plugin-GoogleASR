f837c372785251a39397facbab1230a818857fca
Alexander Koller <koller@ling.uni-potsdam.de>
2018-10-03 11:10:18 +0200

    changed visibility of all implementations of abstract methods to public

--- src/main/java/minimalIO/TextOutputNode.java
+++ src/main/java/minimalIO/TextOutputNode.java
@@ -1,19 +1,9 @@
 package minimalIO;
 
 import de.saar.coli.dialogos.marytts.plugin.Resources;
-import com.clt.diamant.ExecutionLogger;
-import com.clt.diamant.IdMap;
-import com.clt.diamant.InputCenter;
-import com.clt.diamant.WozInterface;
-import com.clt.diamant.graph.Node;
 import com.clt.diamant.graph.nodes.AbstractOutputNode;
 import com.clt.speech.SpeechException;
 import com.clt.speech.tts.VoiceName;
-import com.clt.xml.XMLWriter;
-
-import javax.swing.*;
-import java.awt.*;
-import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -44,17 +34,17 @@ public class TextOutputNode extends AbstractOutputNode {
     }
 
     @Override
-    protected IPromptType getDefaultPromptType() {
+    public IPromptType getDefaultPromptType() {
         return PromptType.text;
     }
 
     @Override
-    protected List<VoiceName> getAvailableVoices() {
+    public List<VoiceName> getAvailableVoices() {
         return Collections.singletonList(new VoiceName("", null));
     }
 
     @Override
-    protected void speak(String prompt, Map<String, Object> properties) throws SpeechException {
+    public void speak(String prompt, Map<String, Object> properties) throws SpeechException {
         System.out.println("speech output: " + prompt);
     }
 
@@ -64,7 +54,7 @@ public class TextOutputNode extends AbstractOutputNode {
     }
 
     @Override
-    protected void stopSynthesis() {
+    public void stopSynthesis() {
         // nothing to be done for text output
         System.err.println("being asked to stop speaking");
     }
